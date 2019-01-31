779bd11b7c6f8b524d70c4b743982a67fadb044e
jack <brainard@my.lanecc.edu>
2019-01-31 13:00:12 -0800

    File Updates
    Updated the files with most recent code

--- widgetScripts.js
+++ widgetScripts.js
@@ -1,54 +1,75 @@
+               function displayButtonTestData()
+               {
+                   var searchKey = document.getElementById("searchInputBox").value;
+                   var searchResultsJSON = getSearchResults(searchKey).then(function(json)
+                                             {
+                                                var parsedResults = renderHtml(json);
+                   
+                                               document.getElementById("outputLabel").innerHTML = parsedResults;
+                                               console.log(parsedResults);
+                                             });
+               }
+                
+               function renderHtml(json)
+                {
+                   var parsedResults="";
+                   for(i = 0; i<json.length; i++)
+                   {
+                       var cResult = json[i];
+                       var prodName = cResult.Name;
+                       var prodId = cResult.Id;
+                       var prodHtml="<a href='/"+prodId+"'>"+ prodName + "ID:"+prodId+"</a><br>";
+                       parsedResults += prodHtml;
+                   }
+                    return parsedResults;
+                }
+				
+				function init()
+				{
+					var searchWidget = document.getElementById("searchWidget");
+					function checkEvent(e){
 
-function displayButtonTestData()
-{
-    var searchKey = document.getElementById("searchInputBox").value;
-    var searchResultsJSON = getSearchResults(searchKey).then(function(json)
-    {
-        var parsedResults = renderHtml(json);
-        document.getElementById("outputLabel").innerHTML = parsedResults;
-        console.log(parsedResults);
-    });
-}
-            	
-function renderHtml(json)
-{
-    var parsedResults = "";
-    for(i = 0; i<json.length; i++)
-    {
-        var cResult = json[i];
-        var prodName = cResult.Name;
-        var prodId = cResult.Id;
-        var prodHtml="<a href='/"+prodId+"' >"+ prodName + "ID:"+prodId+"</a><br>";
-        parsedResults += prodHtml;
-    }
-        return parsedResults;
- }
-           
-function getSearchResults(string) 
-{
-    return new Promise(function(resolve,reject)
-    {
-        HelloWorld.GetQueryResults(string,function(result, event){
-        console.log(result);
-        console.log(event);
-        if (event.status)
-        {
-            resolve(result);
-        }
-        else
-        {
-            reject(result,event);
-        }
-        },
-        {escape: true});
-       	});
-}
-function checkEvent{
-    alert = "you clicked me."
-}
-function Init()
-{
-    var searchWidget = getElementById("searchWidget");
-    searchWidget.addEventListener("click",checkEvent,true);
-}
+						e.stopPropagation();
+e.preventDefault();
 
+// Reference to clicked element
+var target = e.target;
+var href = target.getAttribute('href');
+
+// A, DIV, BUTTON
+var elemType = target.nodeName; // => A
+
+alert('you clicked me!');
+
+// I think... maybe.... could be wrong
+window.location = href;
+
+return false;
+}
+					searchWidget.addEventListener("click", checkEvent,true);
+					
+					
+					
+				}
+				
+                function getSearchResults(string) 
+                    {
+                        return new Promise(function(resolve,reject)
+                            {
+                                HelloWorld.GetQueryResults(string,function(result, event){
+                                console.log(result);
+                                console.log(event);
+                                if (event.status)
+                                {
+                                resolve(result);
+                                }
+                                else
+                                {
+                                reject(result,event);
+                                }
+                                },
+                                {escape: true});
+                                });
+                                }
+								
+							}
