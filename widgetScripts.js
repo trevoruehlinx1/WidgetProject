4485b2a486ed8998a0516ccb14d41ca8f5ebeb99
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2019-01-31 13:14:27 -0800

    cleaned up code

--- widgetScripts.js
+++ widgetScripts.js
@@ -1,75 +1,69 @@
-               function displayButtonTestData()
-               {
-                   var searchKey = document.getElementById("searchInputBox").value;
-                   var searchResultsJSON = getSearchResults(searchKey).then(function(json)
-                                             {
-                                                var parsedResults = renderHtml(json);
+function displayButtonTestData()
+{
+    var searchKey = document.getElementById("searchInputBox").value;
+    var searchResultsJSON = getSearchResults(searchKey).then(function(json)
+    {
+        var parsedResults = renderHtml(json);
                    
-                                               document.getElementById("outputLabel").innerHTML = parsedResults;
-                                               console.log(parsedResults);
-                                             });
-               }
-                
-               function renderHtml(json)
-                {
-                   var parsedResults="";
-                   for(i = 0; i<json.length; i++)
-                   {
-                       var cResult = json[i];
-                       var prodName = cResult.Name;
-                       var prodId = cResult.Id;
-                       var prodHtml="<a href='/"+prodId+"'>"+ prodName + "ID:"+prodId+"</a><br>";
-                       parsedResults += prodHtml;
-                   }
-                    return parsedResults;
-                }
-				
-				function init()
-				{
-					var searchWidget = document.getElementById("searchWidget");
-					function checkEvent(e){
-
-						e.stopPropagation();
-e.preventDefault();
-
-// Reference to clicked element
-var target = e.target;
-var href = target.getAttribute('href');
-
-// A, DIV, BUTTON
-var elemType = target.nodeName; // => A
-
-alert('you clicked me!');
-
-// I think... maybe.... could be wrong
-window.location = href;
-
-return false;
+        document.getElementById("outputLabel").innerHTML = parsedResults;
+        console.log(parsedResults);
+    });
 }
-					searchWidget.addEventListener("click", checkEvent,true);
-					
-					
-					
-				}
+                
+function renderHtml(json)
+{
+    var parsedResults="";
+    for(i = 0; i<json.length; i++)
+        {
+            var cResult = json[i];
+            var prodName = cResult.Name;
+            var prodId = cResult.Id;
+            var prodHtml="<a href='/"+prodId+"'>"+ prodName + "ID:"+prodId+"</a><br>";
+            parsedResults += prodHtml;
+        }
+            return parsedResults;
+}
 				
-                function getSearchResults(string) 
-                    {
-                        return new Promise(function(resolve,reject)
-                            {
-                                HelloWorld.GetQueryResults(string,function(result, event){
-                                console.log(result);
-                                console.log(event);
-                                if (event.status)
-                                {
-                                resolve(result);
-                                }
-                                else
-                                {
-                                reject(result,event);
-                                }
-                                },
-                                {escape: true});
-                                });
-                                }
-								
-							}
+function init()
+{
+	var searchWidget = document.getElementById("searchWidget");
+    function checkEvent(e)
+    {
+        e.stopPropagation();
+        e.preventDefault();
+        // Reference to clicked element
+        var target = e.target;
+        var href = target.getAttribute('href');
+
+        // A, DIV, BUTTON
+        var elemType = target.nodeName; // => A
+
+        alert('you clicked me!');
+
+        // I think... maybe.... could be wrong
+        window.location = href;
+
+        return false;
+    }
+		searchWidget.addEventListener("click", checkEvent,true);
+}
+				
+function getSearchResults(string) 
+{
+    return new Promise(function(resolve,reject)
+        {
+            HelloWorld.GetQueryResults(string,function(result, event){
+            console.log(result);
+            console.log(event);
+            if (event.status)
+            {
+            resolve(result);
+            }
+            else
+            {
+            reject(result,event);
+            }
+            },
+            {escape: true});
+            });
+}
